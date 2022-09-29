e0fc95181c693906a667ac3da4231397db58cf32
Latgardi <saikinmirai@gmail.com>
2022-09-29 18:35:42 +0300

    ex2-93

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,6 +1,6 @@
 <?php
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("EventHandlers", "deactivateBlockElement"));
-
+AddEventHandler("main", "OnEpilog", Array("EventHandlers", "writeLog404Handler"));
 class EventHandlers
 {
 	public function deactivateBlockElement(array &$arFields)
@@ -12,6 +12,20 @@ class EventHandlers
 				$APPLICATION->throwException("Товар невозможно деактивировать, у него $count просмотров");
 				return false;
 			}
+		}
+	}
+
+	public function writeLog404Handler()
+	{
+		if (defined("ERROR_404")) {
+			global $APPLICATION;
+			$pageURL = $APPLICATION->GetCurPage();
+			CEventLog::Add(array(
+				"SEVERITY" => "SECURITY",
+				"AUDIT_TYPE_ID" => "ERROR_404",
+				"MODULE_ID" => "MAIN",
+				"DESCRIPTION" => $pageURL
+			));
 		}
 	}
 }
